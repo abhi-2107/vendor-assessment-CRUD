21626995f9494ce2773a89ef309abbc4d3221b69
Abhishek panchal <abhipan.ap33@gmail.com>
2025-07-16 10:22:33 +0530

    project setup with, react, tailwind, shadcn

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl">Let's begin</h1>
+        <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
